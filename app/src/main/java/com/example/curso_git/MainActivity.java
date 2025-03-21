b6f58319f6bd202a8d53ef0f0232cdb312e8d45f
dannyRT21 <USSS025824@ugb.edu.sv>
2025-03-21 10:01:22 -0600

    A ver si me da??

--- app/src/main/java/com/example/curso_git/MainActivity.java
+++ app/src/main/java/com/example/curso_git/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.curso_git;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,7 +9,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
